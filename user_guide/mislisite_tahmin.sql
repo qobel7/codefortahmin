37ff62e785ff92a0763450a1a7d102bac1ff57fd
caner kavak <caner.kavak@outlook.com>
2019-10-04 01:56:27 +0300

    add db

---
+++ user_guide/mislisite_tahmin.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: localhost:3306
+-- Üretim Zamanı: 03 Eki 2019, 23:40:03
+-- Sunucu sürümü: 10.3.18-MariaDB
+-- PHP Sürümü: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `mislisite_tahmin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(40) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `data` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `guess`
+--
+
+CREATE TABLE `guess` (
+  `id` int(10) NOT NULL,
+  `match_time` text COLLATE utf8_turkish_ci NOT NULL,
+  `sides` text COLLATE utf8_turkish_ci NOT NULL,
+  `guess_percent` text COLLATE utf8_turkish_ci NOT NULL,
+  `guess` text COLLATE utf8_turkish_ci NOT NULL,
+  `credit` text COLLATE utf8_turkish_ci NOT NULL,
+  `guess_state` text COLLATE utf8_turkish_ci NOT NULL,
+  `guess_code` text COLLATE utf8_turkish_ci NOT NULL,
+  `state` text COLLATE utf8_turkish_ci NOT NULL,
+  `description` text COLLATE utf8_turkish_ci NOT NULL,
+  `match_code` text COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo için tablo yapısı `guess_users`
+--
+
+CREATE TABLE `guess_users` (
+  `id` int(11) NOT NULL,
+  `name` text COLLATE utf8_turkish_ci NOT NULL,
+  `email` text COLLATE utf8_turkish_ci NOT NULL,
+  `password` text COLLATE utf8_turkish_ci NOT NULL,
+  `credit` text COLLATE utf8_turkish_ci NOT NULL,
+  `is_login` text COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo için tablo yapısı `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(10) NOT NULL,
+  `text` text COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(255) COLLATE utf8_turkish_ci NOT NULL DEFAULT '',
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `password` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `avatar` varchar(255) CHARACTER SET latin1 DEFAULT 'default.jpg',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_admin` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+  `is_confirmed` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+  `is_deleted` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+  `changecode` varchar(100) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ci_sessions_timestamp` (`timestamp`);
+
+--
+-- Tablo için indeksler `guess`
+--
+ALTER TABLE `guess`
+  ADD KEY `id` (`id`),
+  ADD KEY `id_2` (`id`),
+  ADD KEY `id_3` (`id`);
+
+--
+-- Tablo için indeksler `guess_users`
+--
+ALTER TABLE `guess_users`
+  ADD KEY `id` (`id`);
+
+--
+-- Tablo için indeksler `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Tablo için indeksler `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `guess`
+--
+ALTER TABLE `guess`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=406;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `guess_users`
+--
+ALTER TABLE `guess_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
